8a95c2590110b3b65bd51e622d15aa0ba0a51c28
lucasolasz <olaszlucas072@gmail.com>
2022-07-24 19:54:01 -0300

    V_1.3.1 - Coloca imagem na página Sobre

--- app/Views/paginas/sobre.php
+++ app/Views/paginas/sobre.php
@@ -2,7 +2,24 @@
     <div class="row">
         <div class="col-md-8">
             <h1><?= $dados['tituloPagina'] ?></h3>
-            <p>A moradia própria é um resultado de necessidade + sonho, o meu desejo é ser um instrumento para que este resultado aconteça para você, conte comigo</p>
+                <p>A moradia própria é um resultado de necessidade + sonho, o meu desejo é ser um instrumento para que este resultado aconteça para você, conte comigo</p>
         </div>
     </div>
+    <div class="row">
+        <div class="col">
+            <div class="card border-0">
+                <div class="card-body text-center">
+                    <div class="feature">
+                        <picture>
+                            <img src="<?= URL . "/img/fotoPerfil.jpeg" ?>" alt="fotoPerfil" class="img-fluid rounded-circle imgPerfil">
+                        </picture>
+                        <h2>Corretor</h2>
+                        <p>Rogério Vanderlei Maia do Amaral </p>
+                        <p>CRECI 088.041</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </div>
