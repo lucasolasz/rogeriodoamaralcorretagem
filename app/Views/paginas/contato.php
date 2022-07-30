9d7c1422d49a2667da135ea3840dd7f6b8684977
lucasolasz <olaszlucas072@gmail.com>
2022-07-30 11:52:44 -0300

    V_1.3.13 - Ajuste de link de contato

--- app/Views/paginas/contato.php
+++ app/Views/paginas/contato.php
@@ -2,20 +2,22 @@
 // var_dump($dados);
 $celular = "5521960146947";
 
+//link da aplicação que auxiliou em abrir os links https://www.wppredirect.tk/
+
 ?>
 
-<div class="container py-4">    
+<div class="container py-4">
     <div class="row align-items-md-stretch">
         <div class="col-md-6">
             <div class="h-100 p-5 bg-light rounded-3">
                 <h2>Contato</h2>
-                
-                <p class="mt-3">Entre em contato via Whatsapp. Clique no ícone abaixo</p>
+
+                <p class="mt-3">Entre em contato via Whatsapp. Clique no ícone abaixo</p>               
 
                 <div class="d-flex justify-content-center mt-3 quadrado-whats">
-                    <a href="https://web.whatsapp.com/send?phone=<?=$celular?>" target="_blank" class="btn text-start"><img src="<?= URL . '/img/whatsapp.png' ?>" alt="" class="whats"></a>
+                    <a href="https://www.wppredirect.tk/go/?p=<?= $celular ?>&m=Ol%C3%A1,%20gostaria%20de%20conversar%20a%20respeito%20de%20um%20im%C3%B3vel" target="_blank" class="btn text-start"><img src="<?= URL . '/img/whatsapp.png' ?>" alt="" class="whats"></a>
                 </div>
-                
+
             </div>
 
         </div>
--- app/Views/paginas/envioContato.php
+++ app/Views/paginas/envioContato.php
@@ -5,9 +5,11 @@ $celular = "5521960146947";
 $dataEscolhida = Checa::dataBr($dados['txtDataHidden']);
 $horaEscolhida = Checa::horaFormat($dados['txtHoraHidden']);
 
-$texto = " Olá, meu nome é {$dados['txtNomeContato']}, \nPossuo o E-mail: {$dados['txtEmailContato']}, Telefone: {$dados['txtTelefoneContato']} \n\n
-Acabo de solicitar um agendamento para o Imóvel: \n
-Id= ##{$dados['imovel']->id_imovel} \nData = {$dataEscolhida} \nHora = {$horaEscolhida}h \nEndereço: {$dados['imovel']->ds_rua_imovel}, {$dados['imovel']->ds_bairro} \n\nObrigado!";
+$texto = "Olá, meu nome é {$dados['txtNomeContato']}, %0APossuo o E-mail: {$dados['txtEmailContato']}, %0ATelefone: {$dados['txtTelefoneContato']} %0A
+Acabo de solicitar um agendamento para o Imóvel: %0A
+Id= ##{$dados['imovel']->id_imovel} %0AData = {$dataEscolhida} %0AHora = {$horaEscolhida}h %0AEndereço: {$dados['imovel']->ds_rua_imovel}, {$dados['imovel']->ds_bairro}%0A
+%0AEste horário está disponível?
+%0A%0AObrigado!";
 
 $textoEncoded = urlencode($texto);
 
@@ -22,11 +24,9 @@ $textoEncoded = urlencode($texto);
             <div class="h-100 p-5 bg-light rounded-3">
                 <h2>Tudo certo!</h2>
                 <h3 class="transparente">Clique no botão abaixo para finalizar seu contato:</h3>
-                
-                
-                
+
                 <div class="d-flex justify-content-center mt-5 quadrado-whats">
-                    <a href="https://web.whatsapp.com/send?phone=<?=$celular . '&text=' . $textoEncoded ?>" target="_blank" class="btn text-start"><img src="<?= URL . '/img/whatsapp.png' ?>" alt="" class="whats"></a>
+                    <a href="https://www.wppredirect.tk/go/?p=<?= $celular . '&m=' . $textoEncoded ?>" target="_blank" class="btn text-start"><img src="<?= URL . '/img/whatsapp.png' ?>" alt="" class="whats"></a>
                 </div>
 
 
