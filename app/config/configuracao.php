87357eef72e309af336c90078c44dbf87420fbd9
lucasolasz <olaszlucas072@gmail.com>
2022-07-31 00:23:10 -0300

    Ajusta arquivos de configuração para hospedagem

--- app/config/configuracao.php
+++ app/config/configuracao.php
@@ -1,16 +1,25 @@
 <?php 
 
 //Parametros para o Banco
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'amaralcorretagem');
+// define('DB_PORT', '3306');
+
+//Parametros para o Banco
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'amaralcorretagem');
+define('DB_USER', 'sfsyst94_id18241842');
+define('DB_PASS', '1X5l5DnM!E');
+define('DB_NAME', 'sfsyst94_amaralcorretagem');
 define('DB_PORT', '3306');
 
 //dirname() retorna o nome do diretorio em que o arquivo está
 define('APP', dirname(dirname(__FILE__)));
 
-define('URL', '/rogeriodoamaralcorretagem');
+// define('URL', '/rogeriodoamaralcorretagem');
+
+define('URL', 'https://rogeriodoamaralcorretagem.com.br');
 
 define('APP_NOME', 'Rogério do Amaral - Corretagem');
 
